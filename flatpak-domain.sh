b575235b776f43b90e7db036220bab181b5de814
Daniel Hast <hast.daniel@protonmail.com>
2025-08-06 21:09:34 -0400

    feat!: initial draft of flatpak confinement policy
    Currently flatpak is not compatible with SELinux confined users: the
    flatpak executable does not have the necessary permissions to create the
    sandbox. This policy solves this by confining the flatpak program itself
    (and the helper programs it calls, such as bwrap) in an application
    domain with the necessary permissions, which transitions back into the
    user domain to run the sandboxed flatpak application.
    
    For example, if a `staff_u` user runs a flatpak application from the
    user domain `staff_t`, flatpak will run in `staff_flatpak_t`, while the
    app will run in `staff_t`.
    
    The policy also includes more fine-grained labeling for flatpak app
    library files, data files, cache files, and temporary files.

---
+++ flatpak-domain.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Building and Loading Policy"
+
+set -x
+
+make -f /usr/share/selinux/devel/Makefile flatpak-domain.pp || exit
+/usr/sbin/semodule -i flatpak-domain.pp
+
+/sbin/restorecon -FRiv /usr/bin /usr/libexec /var/lib/flatpak
+/sbin/restorecon -FRiv /home/*/.local/share/flatpak /home/*/.cache/flatpak /home/*/.var
+/sbin/restorecon -FRiv /root/.local/share/flatpak /root/.cache/flatpak /root/.var
