3879fe589b85568b19d8ae3cdb688d6d0c6cc651
Daniel Hast <hast.daniel@protonmail.com>
2025-08-18 16:22:19 -0400

    add flatpak_user_tmp_t type, reorder rules for more consistent style

--- flatpak-domain.sh
+++ flatpak-domain.sh
@@ -10,3 +10,4 @@ make -f /usr/share/selinux/devel/Makefile flatpak-domain.pp || exit
 /sbin/restorecon -FRiv /usr/bin /usr/libexec /var/lib/flatpak
 /sbin/restorecon -FRiv /home/*/.local/share/flatpak /home/*/.cache/flatpak /home/*/.var
 /sbin/restorecon -FRiv /root/.local/share/flatpak /root/.cache/flatpak /root/.var
+/sbin/restorecon -FRiv /run/user/*/{app,.flatpak,.flatpak-helper}
